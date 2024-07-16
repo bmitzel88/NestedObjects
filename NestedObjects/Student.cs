c335c48664fb36710fad816a0342f311a80e6d9d
Brandon <brandonmitzel05@gmail.com>
2024-07-16 15:09:04 -0700

    Created student and advisor classes

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a individual college student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Legal first name of Student
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Legal first name of Student
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; private set; }
+
+        /// <summary>
+        /// The student's date of birth (date only)
+        /// </summary>
+        public DateOnly DateOfBirth { get; set; }
+
+        /// <summary>
+        /// The student's primary phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// The students assigned advisor
+        /// </summary>
+        public Advisor AssignedAdvisor { get; set; }
+
+
+
+    }
+}
